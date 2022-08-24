036a6d9e39494452c0db5b19790e7afa85061aee
Alicia Charef <alcharef@gmail.com>
2022-08-24 19:57:22 +0200

    first commit

---
+++ assets/js/app.js
@@ -0,0 +1,29 @@
+/* MENU DEROULANT */
+let btnmenu = document.querySelector('.navbar-toggler')
+let element = document.querySelector('#navbarCollapse')
+
+btnmenu.addEventListener('click', navbar)
+function navbar() {
+  if (element.style.display == 'block') {
+    element.style.display = 'none'
+  } else {
+    element.style.display = 'block'
+  }
+}
+
+/* ALEATOIRE IMAGE */
+
+let btnafficher = document.querySelector('.button');
+let dogresult = document.querySelector('#dog_result');
+
+
+btnafficher.addEventListener('click', getRandomDog)
+
+function getRandomDog(){
+
+fetch('https://dog.ceo/api/breeds/image/random')
+.then(res=> res.json())
+.then(data => dogresult.innerHTML = '<img src = ${data.message}"/>'
+)
+
+}
